ffaeb5909a7b1d825991c2320a3d2518a612806a
David Groulx <david@sandbendersoftware.com>
2013-03-07 13:42:55 -0500

    Make clean_neo4j a Module method, not ment to be mixed in

--- lib/neo4j_dev_rails.rb
+++ lib/neo4j_dev_rails.rb
@@ -4,7 +4,7 @@ require 'rest_client'
 module Neo4jDevRails
   require File.expand_path('../neo4j_dev_rails/railtie', __FILE__) if defined?(Rails)
 
-  def clean_neo4j
+  def self.clean_neo4j
     response = RestClient.post 'http://localhost:7574/db/data/cypher', { query: 'START n0=node(0),nx=node(*) MATCH n0-[r0?]-(),nx-[rx?]-() WHERE nx <> n0 DELETE r0,rx,nx' }.to_json, accept: :json, content_type: :json
     response.code == 200
   end
