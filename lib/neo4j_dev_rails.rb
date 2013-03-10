65e251bc640f697d47cf77630536e77ee055b108
David Groulx <david@sandbendersoftware.com>
2013-03-09 21:50:53 -0500

    Rearrange directory structure for rake tasks, update documentation

--- lib/neo4j_dev_rails.rb
+++ lib/neo4j_dev_rails.rb
@@ -5,7 +5,7 @@ module Neo4jDevRails
   require File.expand_path('../neo4j_dev_rails/railtie', __FILE__) if defined?(Rails)
 
   def self.clean_neo4j
-    response = RestClient.post "#{test_host}:#{test_port}/db/data/cypher", { query: 'START n0=node(0),nx=node(*) MATCH n0-[r0?]-(),nx-[rx?]-() WHERE nx <> n0 DELETE r0,rx,nx' }.to_json, accept: :json, content_type: :json
+    response = RestClient.post "#{test_host}:#{test_port.to_s}/db/data/cypher", { query: 'START n0=node(0),nx=node(*) MATCH n0-[r0?]-(),nx-[rx?]-() WHERE nx <> n0 DELETE r0,rx,nx' }.to_json, accept: :json, content_type: :json
     response.code == 200
   end
 
--- lib/neo4j_dev_rails/railtie.rb
+++ lib/neo4j_dev_rails/railtie.rb
@@ -1,7 +1,7 @@
 module Neo4jDevRails
   class Railtie < Rails::Railtie
     rake_tasks do
-      require File.expand_path('../../tasks/neo4j/neo4j', __FILE__)
+      require File.expand_path('../../tasks/neo4j', __FILE__)
     end
   end
 end
