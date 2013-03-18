cbf4e7d17fcc06d23b3eac5b0cf1eb390a0bd097
David Groulx <david@sandbendersoftware.com>
2013-03-18 18:49:30 -0400

    Remove all node indicies when cleaning out the database

--- lib/neo4j_dev_rails/test.rb
+++ lib/neo4j_dev_rails/test.rb
@@ -13,13 +13,13 @@ module Neo4jDevRails
       req = Net::HTTP::Post.new('/db/data/cypher', {'Accpet' => 'application/json', 'Content-Type' => 'application/json'})
       req.body = { query: 'START n0=node(0),nx=node(*) MATCH n0-[r0?]-(),nx-[rx?]-() WHERE nx <> n0 DELETE r0,rx,nx' }.to_json
       res = http.request(req)
-      return false unless res.code == 200
 
       # Clean out node indicies
-      req = Net::HTTP::Get.new '/db/data/index/node/', {'Accept' => 'application/json'}
+      req = Net::HTTP::Get.new '/db/data/index/node', {'Accept' => 'application/json'}
       res = http.request(req)
-      JSON.parse(response.body).each do |index|
-        req = Net::HTTP::Delete.new '/db/data/index/node/#{index.first}', {'Application' => 'application/json'}
+      JSON.parse(res.body).each do |index|
+        req = Net::HTTP::Delete.new "/db/data/index/node/#{index.first}", {'Application' => 'application/json'}
+        res = http.request(req)
       end
     end
 
